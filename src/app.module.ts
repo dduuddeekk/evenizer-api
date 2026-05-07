288f93866c973dc8297f135b35c1a871e8835f10
dduuddeekk <jajajablablabla123@gmail.com>
2026-05-07 14:56:52 +0800

    chores: ticker start

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UploadModule } from './upload/upload.module';
 import { EventModule } from './event/event.module';
 import { OrganizerModule } from './organizer/organizer.module';
+import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, UploadModule, EventModule, OrganizerModule],
+  imports: [UserModule, AuthModule, PrismaModule, UploadModule, EventModule, OrganizerModule, TicketModule],
   controllers: [AppController],
   providers: [AppService],
 })
